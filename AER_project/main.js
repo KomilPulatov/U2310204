f014ba50c83d1c3e99897d1220e9712238edc2ff
Olim <olim@gmail.com>
2023-11-26 20:33:07 +0500

    fix

--- AER_project/main.js
+++ AER_project/main.js
@@ -11,17 +11,32 @@ fetch(filePath)
 function startGame() {
   let questions = jsonFile.questions;
   let question = questions[question_id];
-  let questionText = question.question;
+  let questionText = question.question_text;
   let answers = question.answers;
   let questionElement = document.getElementById("question");
   questionElement.innerHTML = questionText;
   for (let i = 0; i < answers.length; i++) {
     let answerElement = document.getElementById(`answer${i}`);
+    answerElement.style.backgroundColor = "white";
     answerElement.innerHTML = answers[i].text;
   }
   question_id++;
   if (question_id >= questions.length) {
     let buttonElement = document.getElementById("NEXT");
     buttonElement.disabled = true;
+  }
+}
+
+function showAnswer() {
+  let questions = jsonFile.questions;
+  let question = questions[question_id - 1];
+  let answers = question.answers;
+  for (let i = 0; i < answers.length; i++) {
+    let answerElement = document.getElementById(`answer${i}`);
+    if (answers[i].correct) {
+      answerElement.style.backgroundColor = "green";
+    } else {
+      answerElement.style.backgroundColor = "red";
+    }
   }
 }
